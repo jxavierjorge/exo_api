1befde8ac740fdb14aeacdd1a7366dd0cf7fa10f
jxavi <jxavierjorge@gmail.com>
2024-07-25 11:35:19 -0300

    Att ProjetoRepo

--- Exo.WebApi/obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
+++ Exo.WebApi/obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72a7d47eddd7b52c9c8af0154d999ba8edf455bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
